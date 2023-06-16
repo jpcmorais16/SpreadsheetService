33eba202a10eec6a60713ec911b979bbc574dab5
jpcmorais16 <jpcmorais2001@gmail.com>
2023-06-15 23:00:56 -0300

    organizing

--- Worker/Program.cs
+++
@@ -1,7 +0,0 @@
-using Worker;
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker.Worker>(); })
-    .Build();
-
-await host.RunAsync();
